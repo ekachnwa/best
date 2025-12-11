e22437d13dbb398676b7aedadc2a7128acada186
ekachnwa <onyekachi.nwaogwu@gmail.com>
2025-12-11 17:56:50 +0100

    updated projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -12,7 +12,7 @@ export const Projects = () => {
                             <p className="text-gray-400 mb-4">Designed modern responsive user interface for mobile and web app using Figma design tool.</p>
 
                             <div className="flex flex-wrap gap-2 mb-4">
-                                {["Figma"].map((tech, key) =>  <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bgoblue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                {["Figma","Figmotion","Iconify"].map((tech, key) =>  <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bgoblue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
                                         {tech}</span>)}
                             </div>
                             <div className="flex justify-between items-center">
@@ -21,11 +21,11 @@ export const Projects = () => {
                         </div>
 
                         <div className="p-6 rounded-xl border border-white/10 hover:bgoblue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
-                            <h3 className="text-xl font-bold mb-4">Cloud Platform</h3>
-                            <p className="text-gray-400 mb-4">Scalable cloud infrastructure management with real-time monitoring and automated scaling.</p>
+                            <h3 className="text-xl font-bold mb-4">Web App</h3>
+                            <p className="text-gray-400 mb-4">Scalable web application infrastructure with real-time monitoring and automated scaling.</p>
 
                             <div className="flex flex-wrap gap-2 mb-4">
-                                {["React", "Node.js", "AWS", "Docker"].map((tech, key) =>  <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bgoblue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                {["React", "PHP"].map((tech, key) =>  <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bgoblue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
                                         {tech}</span>)}
                             </div>
                             <div className="flex justify-between items-center">
@@ -34,11 +34,11 @@ export const Projects = () => {
                         </div>
 
                         <div className="p-6 rounded-xl border border-white/10 hover:bgoblue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
-                            <h3 className="text-xl font-bold mb-4">Cloud Platform</h3>
-                            <p className="text-gray-400 mb-4">Scalable cloud infrastructure management with real-time monitoring and automated scaling.</p>
+                            <h3 className="text-xl font-bold mb-4">School Portal</h3>
+                            <p className="text-gray-400 mb-4">A comprehensive school portal with integrated student management, grade tracking, and communication tools.</p>
 
                             <div className="flex flex-wrap gap-2 mb-4">
-                                {["React", "Node.js", "AWS", "Docker"].map((tech, key) =>  <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bgoblue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                {[ "WP","React", "PHP"].map((tech, key) =>  <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bgoblue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
                                         {tech}</span>)}
                             </div>
                             <div className="flex justify-between items-center">
@@ -47,15 +47,15 @@ export const Projects = () => {
                         </div>
 
                         <div className="p-6 rounded-xl border border-white/10 hover:bgoblue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
-                            <h3 className="text-xl font-bold mb-4">Cloud Platform</h3>
-                            <p className="text-gray-400 mb-4">Scalable cloud infrastructure management with real-time monitoring and automated scaling.</p>
+                            <h3 className="text-xl font-bold mb-4">E-commerce Platform</h3>
+                            <p className="text-gray-400 mb-4">E-commerce platform with integrated payment processing, inventory management, and user authentication.</p>
 
                             <div className="flex flex-wrap gap-2 mb-4">
-                                {["React", "Node.js", "AWS", "Docker"].map((tech, key) =>  <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bgoblue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
+                                {["React", "Laravel", "VPS"].map((tech, key) =>  <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bgoblue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition">
                                         {tech}</span>)}
                             </div>
                             <div className="flex justify-between items-center">
-                                <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project →</a>
+                                <a href="https://kusnap.com" className="text-blue-400 hover:text-blue-300 transition-colors my-4" target="_blank">View Project →</a>
                             </div>
                         </div>
                         
